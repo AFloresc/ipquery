0d64e1b5be550925df2e1b54a46b8e10343fe483
AFloresc <afloresc@tecnocampus.cat>
2026-07-05 19:03:03 +0200

    feat: Added my logger to router

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
+
+	mylogger "ipquery/internal/logger"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,7 +15,10 @@ import (
 
 // NewRouter configura y retorna el enrutador
 func NewRouter(h *IPHandler) http.Handler {
+
 	r := chi.NewRouter()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	// CORS
 	c := cors.New(cors.Options{
@@ -28,6 +35,8 @@ func NewRouter(h *IPHandler) http.Handler {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Use(mylogger.NewStructuredLogger(logger))
 
 	// Rutas
 	r.Route("/v1", func(r chi.Router) {
